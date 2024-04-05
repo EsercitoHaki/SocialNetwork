3f675e983604e21aa886c21eb00c063d8fda597f
EsercitoHaki <esercitohaki2003@gmail.com>
2024-04-06 04:02:50 +0700

    login

--- app/src/main/java/com/example/socialnetwork/MainActivity.java
+++ app/src/main/java/com/example/socialnetwork/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.socialnetwork;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import com.example.socialnetwork.fragments.LoginFragment;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new LoginFragment())
+                .commit();
     }
 }
--- app/src/main/java/com/example/socialnetwork/fragments/LoginFragment.java
+++ app/src/main/java/com/example/socialnetwork/fragments/LoginFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.socialnetwork.R;
 
@@ -20,6 +22,24 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        EditText usernameEditText = view.findViewById(R.id.userName);
+        EditText passwordEditText = view.findViewById(R.id.password);
+        Button loginButton = view.findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                // In thông tin ra console
+                System.out.println("Username: " + username);
+                System.out.println("Password: " + password);
+            }
+        });
+
+        return view;
     }
 }
